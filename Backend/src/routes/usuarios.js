be64f05de58cdad7fea5ddac43d0eb988f05b6b2
Sneider-Khaos <eternalkhaos2@gmail.com>
2025-09-26 20:18:58 -0600

    correcciones

--- Backend/src/routes/usuarios.js
+++ Backend/src/routes/usuarios.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Usuario = require('../models/JS_Usuario');
+const Usuario = require('../models/Usuario');
 const router = express.Router();
 
 // REGISTRO de usuario
