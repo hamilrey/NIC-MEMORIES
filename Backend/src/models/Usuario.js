be2106e7c8728168af22707727a27d86db71e436
Sneider-Khaos <eternalkhaos2@gmail.com>
2025-09-28 22:33:34 -0600

    nuevo actualizacion

--- Backend/src/models/Usuario.js
+++ Backend/src/models/Usuario.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); // AGREGAR ESTA LÍNEA
 
 const usuarioSchema = new mongoose.Schema({
   nombre: {
@@ -20,4 +21,17 @@ const usuarioSchema = new mongoose.Schema({
   }
 });
 
+// Encriptar password antes de guardar
+usuarioSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Método para comparar passwords
+usuarioSchema.methods.comparePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// SOLO UN module.exports - ELIMINAR EL DUPLICADO
 module.exports = mongoose.model('Usuario', usuarioSchema);
